32d18a04d51b0713764ce97042a5b5b5ff92d066
LHP7-Arnaud7739 <arnaudpesquet@MacBook-Pro-de-Arnaud.local>
2021-11-16 12:23:23 +0100

    Pendu fini

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const myKeyboard = document.getElementById("myKeyboard")
 const petList = ["panda", "lion", "tigre", "ecureuil", "elephant", "cheval", "leopard", "chien", "chat", "kangourou", "loup", "pingouin", "koala", "manchot", "hippotame", "guepard", "zebre", "griffon", "linx"]
-
+let play = true
 
 
 
 let pet = petList[aleaNb(0, petList.length, )]
-console.log(pet)
+
+// console.LOG POUR TRICHER
+ console.log(pet)
 
 
 // CREATION DU CODE POUR CREER UNE BOUCLE A INCLURE DANS LA BOUCLE LIGNE 15 A 20
@@ -52,29 +54,84 @@ arrayPet.forEach((element, index) => {
 })
 
 let nbLetter = 0
-let life = 10
+let life = 8
 
 
 
 
 
 myKeyboard.addEventListener("click", (e) => {
-    console.log(e.target)
-    if (e.target.nodeName == "BUTTON") {
+    // console.log(e.target)
+    if (e.target.nodeName == "BUTTON" && play === true) {
+        let myScore = nbLetter
         console.log(e.target.innerText)
         e.target.className = "m-2 col-2 btn btn-danger"
-e.target.disabled = true
-         arrayPet.forEach((element, index) => {
- if(e.target.innerText.toLowerCase() == element){
-document.getElementById(`indexLetter${index}`)
-.innerText= element
-nbLetter++
- }
+        e.target.disabled = true
+        arrayPet.forEach((element, index) => {
+            if (e.target.innerText.toLowerCase() == element) {
+                document.getElementById(`indexLetter${index}`)
+                    .innerText = element
+                nbLetter++
+
+            } 
         })
-       if (nbLetter == arrayPet.length){
-           window.confirm("Gagner! Voulez-vous rejouer ?")
-       }
+        if (nbLetter == arrayPet.length) {
+            if (confirm("Gagner! Voulez-vous rejouer ?")) {
+                location.reload()
+            }
+            play = false
+        }
+
+        if (myScore == nbLetter) {
+            life--
+            
+
+        }
+        console.log(life)
+        switch (life) {
+
+
+            case 0:
+                
+                document.getElementById("myLife").innerHTML = ""
+                if (confirm("perdu, voulez-vous rejouer ?")) {
+                    location.reload();
+                };
+                play = false;
+                break;
+
+
+            case 7:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ ❤️ ❤️ ❤️ ❤️ ❤️🤍"
+                break
+
+            case 6:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ ❤️ ❤️ ❤️ ❤️ 🤍 🤍"
+                break
+
+            case 5:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ ❤️ ❤️ ❤️ 🤍 🤍 🤍"
+                break
+
+            case 4:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ ❤️ ❤️ 🤍 🤍 🤍 🤍"
+                break
+            case 3:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ ❤️ 🤍 🤍 🤍 🤍 🤍"
+                break
+
+            case 2:
+                document.getElementById("myLife").innerHTML = "❤️ ❤️ 🤍 🤍 🤍 🤍 🤍 🤍"
+                break
+
+            case 1:
+                document.getElementById("myLife").innerHTML = "❤️ 🤍 🤍 🤍 🤍 🤍 🤍 🤍"
+                break
+            default:
+                break;
+        }
+
 
 
     }
-    })
+})
